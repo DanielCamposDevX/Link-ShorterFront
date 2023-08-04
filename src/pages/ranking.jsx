de293f61318c026bd0f3e289785d456d0eca5005
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-08-04 16:25:45 -0300

    feat/design home

---
+++ src/pages/ranking.jsx
@@ -0,0 +1,106 @@
+import styled from 'styled-components'
+import Logo from '../assets/Logo.svg'
+import Trophy from '../assets/Trophy.svg'
+
+export default function Ranking() {
+
+    return (
+        <Page>
+            <Header>
+                <Hbutton style={{ color: '#5D9040' }}>Entrar</Hbutton>
+                <Hbutton>Cadastrar-se</Hbutton>
+            </Header>
+            <Container>
+                <CusImg src={Logo} />
+                <Container>
+                    <span style={{ display: 'flex', alignItems: 'center' }}>
+                        <img src={Trophy} />
+                        <h1>Ranking</h1>
+                    </span>
+                    <RankContainer>
+                        <h1><span>1. Fulaninha</span> - 32 links - 1.703.584 visualizações</h1>
+                        <h1><span>1. Fulaninha</span>- 32 links - 1.703.584 visualizações</h1>
+                        <h1><span>1. Fulaninha</span>- 32 links - 1.703.584 visualizações</h1>
+                        <h1><span>1. Fulaninha</span>- 32 links - 1.703.584 visualizações</h1>
+                        <h1><span>1. Fulaninha</span>- 32 links - 1.703.584 visualizações</h1>
+                    </RankContainer>
+                </Container>
+            </Container>
+            <Footer>
+                <h1>Crie sua conta para usar nosso serviço!</h1>
+            </Footer>
+        </Page>
+    );
+}
+
+const Page = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
+
+const Header = styled.div`
+    height: 8vh;
+    width: 80%;
+    display: flex;
+    justify-content: end;
+    align-items: end;
+    margin-bottom: 20px;
+`
+
+const Hbutton = styled.button`
+    border:none;
+    background-color:transparent;
+    font-size: 14px;
+    line-height: 17.5px;
+`
+
+const Container = styled.div`
+    width: 90%;
+    height: 63vh;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction:column;
+    h1{
+        font-size: 36px;
+        font-weight: 700;
+        line-height: 45px;
+        letter-spacing: 0em;
+        text-align: left;
+        margin-left: 10px;
+    }
+`
+
+const CusImg = styled.img`
+    height: 12vh;
+    margin-bottom:40px;
+`
+
+const RankContainer = styled.div`
+    border: 1px solid rgba(0,0,0,0.25);
+    width: 80%;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0px 4px 24px 0px #78B1591F;
+    margin-top: 30px;
+    h1{
+        font-size: 22px;
+        font-weight: 400;
+        line-height: 28px;
+        letter-spacing: 0em;
+        text-align: left;
+        margin-bottom: 10px;
+    }
+`
+const Footer = styled.div`
+    height: 29vh;
+    h1{
+        font-size: 36px;
+        font-weight: 700;
+        line-height: 45px;
+        letter-spacing: 0em;
+        text-align: left;
+        margin-top: 20px;
+    }
+`
